e0dc0d4a1e9af5909b1b51ca69281b8c66e37a63
emse-p4a-gwu <john.helveston@gmail.com>
2023-09-29 06:43:02 -0400

    working on render-first strategy

--- functions.R
+++ functions.R
@@ -1,28 +1,47 @@
-question <- function(type, id, prompt, choices = NULL) {
-  switch(type,
-         text = shiny::textInput(id, prompt),
-         checkbox = shiny::checkboxGroupInput(id, prompt, choices),
-         radio = shiny::radioButtons(id, prompt, choices),
-         stop(paste("Unknown type:", type))
-  )
+# The question function to render a Shiny input based on question type
+question <- function(type, id, label, choices = NULL, conditional = NULL) {
+  if (is.null(conditional) || (is.reactive(conditional) && isTRUE(conditional()))) {
+    switch(type,
+      text = textInput(id, label),
+      select = selectInput(id, label, choices),
+      checkbox = checkboxGroupInput(id, label, choices),
+      # Add more input types as needed
+    )
+  } else {
+    return(NULL)
+  }
 }
 
-parse_survey_pages <- function(file_path) {
-  # Split the document into pages using `knitr::split_file()`
-  pages_split <- knitr:::split_file(file_path)
+# Example function to get color choices for a respondent
+get_color_choices_for_respondent <- function(id) {
+  # In a real-world scenario, you'd load this from a database or another source
+  responses <- list(
+    '1' = c("Red", "Blue"),
+    '2' = c("Orange", "Green")
+  )
+  responses[[id]]
+}
 
-  pages <- lapply(pages_split, function(page_file) {
-    # Extract R code using `knitr::purl()`
-    r_code <- knitr::purl(page_file, quiet = TRUE)
-    r_code_content <- readLines(r_code, warn = FALSE)
-    unlink(r_code)  # delete the temporary .R file
+prep_survey <- function(qmd_file) {
+  # Capture the HTML output
+  captured_output <- capture.output(quarto::quarto_render(qmd_file, output_format = "revealjs"))
 
-    # Evaluate the R code
-    eval(parse(text = r_code_content))
-  })
+  # Extract the section where HTML starts and ends
+  start_idx <- which(grepl("<!DOCTYPE html>", captured_output))
+  end_idx <- which(grepl("</html>", captured_output))
 
-  # Clean up temporary split files
-  file.remove(pages_split)
+  # If we don't find the markers, return an error
+  if (length(start_idx) == 0 || length(end_idx) == 0) {
+    stop("Failed to identify the start or end of the HTML content.")
+  }
 
-  return(pages)
+  # Combine the output to form the full HTML content
+  full_html <- paste(captured_output[start_idx:end_idx], collapse = "\n")
+
+  # Parse the resulting HTML
+  parsed_html <- xml2::read_html(full_html)
+  sections <- parsed_html %>% rvest::html_nodes("section")
+  html_sections <- lapply(sections, as.character)
+
+  return(html_sections)
 }
