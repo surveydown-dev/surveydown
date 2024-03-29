643ce1576412138da244b166f260e76ff370cddb
Buneabt <buneabt@yahoo.com>
2024-03-29 01:43:02 -0400

    GS4 Learning
    Looked into Sheets creation and appending both important for survey creator and survey user.

--- datauploading.R
+++ datauploading.R
@@ -1,4 +1,19 @@
 library(tidyverse)
 library(here)
+library(googlesheets4)
 
 df <- read_csv(here('surveydown', 'data.csv'))
+
+#So would it be better for the creator to create their own sheet,
+#Then add it here so the csv results for the survey can just be appened.
+#Instead of creating a new survery everytime with gs4_create
+#we do sheet_append()
+
+
+#Practice Creation
+
+Spread1 <- gs4_create("Sheets1", sheets = df) #Creation 
+
+Spread1 %>% 
+  sheet_append(df) #Appends survey result
+
