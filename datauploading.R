127706e67a8922e8941eaba0a82419180f026b94
Buneabt <buneabt@yahoo.com>
2024-04-02 10:29:12 -0400

    Clean Up/Bug Fixing
    Cleaned up the code while also fixing a bug where the col_names would replace actual data past the first initiation. For example, if the matching session ID was in row 20, the previous code would replace rows 19 and 20 with the headers and data of the users personal file. Now this just replaces row 20 with the users survey answers.

--- datauploading.R
+++ datauploading.R
@@ -4,23 +4,19 @@ library(googlesheets4)
 
 df <- read_csv(here('surveydown', 'data.csv'))
 
-#So would it be better for the creator to create their own sheet,
-#Then add it here so the csv results for the survey can just be appened.
-#Instead of creating a new survery everytime with gs4_create
-#we do sheet_append()
+sheet_url <- "https://docs.google.com/spreadsheets/d/19S2CJ73N7ISJz8HyO7oclxvKV_N_OAEl5wJE_yQ8VGg/edit#gid=339310633"
 
+Sheet <- read_sheet(sheet_url, range = "A:A")
 
-#Practice Creation
-
-Spread1 <- gs4_create("Sheets1", sheets = df) #Creation 
-
-Sheet <- read_sheet(Spread1, range = "A:A")
+#Spread1 <- gs4_create("Sheets1", sheets = df) #Creation / Only needs to be done once
 
 rows_to_delete <- which(Sheet$session_id %in% df$session_id)
 
-range_write(Spread1, data = df, range = paste0("A", as.character(rows_to_delete)))
+range_write(sheet_url, data = df, range = paste("A", as.character(rows_to_delete[1] + 1), sep = ""), col_names = FALSE) #We have to add one so we can avoid pasting the header
 
-#Things to Do, make it so other people can actually edit the rows instead of having direct
-#access like I do
+
+
+
+
 
 
