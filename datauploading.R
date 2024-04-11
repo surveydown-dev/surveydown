2f39276f17bbaad17ccd5ab6eec674c030f5d0c5
Buneabt <buneabt@yahoo.com>
2024-04-11 14:44:28 -0400

    Update datauploading.R

--- datauploading.R
+++ datauploading.R
@@ -4,20 +4,32 @@ library(googlesheets4)
 
 df <- read_csv(here('data.csv'))
 
-(original_client <- gs4_oauth_client())
-
-(original_api_key <- gs4_api_key())
-
-path_to_json <- system.file(
-  "extdata", "secret.json",
-  package = "gargle"
-)
-gs4_auth_configure(path = path_to_json)
+credentials <- jsonlite::read_json("secret.json")
 
 
+# Replace these values with your actual credentials
+#client_id <- credentials$web$client_id
+#client_secret <- credentials$web$client_secret
+#refresh_token <- credentials$web$token_uri
+#
+# Authenticate with Google Sheets using OAuth credentials
+#gs4_auth(
+#  email = gargle::gargle_oauth_email(),
+#  path = NULL,
+#  subject = NULL,
+#  scopes = "spreadsheets",
+#  cache = gargle::gargle_oauth_cache(),
+#  use_oob = gargle::gargle_oob_default(),
+#  token = NULL
+#)
 
 
-sheet_url <- "https://docs.google.com/spreadsheets/d/19S2CJ73N7ISJz8HyO7oclxvKV_N_OAEl5wJE_yQ8VGg/edit#gid=339310633"
+
+
+#Everything Below is meant for Row editing
+
+
+sheet_url <- "https://docs.google.com/spreadsheets/d/12f5iLaXlXqkWz3pWs6fLLPX6PfiiVxuQ_BzJqKfgacQ/edit#gid=798378927"
 
 Sheet <- read_sheet(sheet_url, range = "A:A")
 
@@ -25,7 +37,11 @@ Sheet <- read_sheet(sheet_url, range = "A:A")
 
 rows_to_delete <- which(Sheet$session_id %in% df$session_id)
 
-range_write(sheet_url, data = df, range = paste("A", as.character(rows_to_delete[1] + 1), sep = ""), col_names = FALSE) #We have to add one so we can avoid pasting the header
+if(length(rows_to_delete) == 0) { #This is meant for first time addition of a row
+  range_write(sheet_url, data = df, range = paste("A", as.character(nrow(Sheet) + 2), sep = ""), col_names = FALSE)
+} else {
+  range_write(sheet_url, data = df, range = paste("A", as.character(rows_to_delete[1] + 1), sep = ""), col_names = FALSE) #We have to add one so we can avoid pasting the header
+}
 
 
 
