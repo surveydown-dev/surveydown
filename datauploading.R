f056bbcfb238b5244eab5fc4fff19201e8b1d92a
Buneabt <buneabt@yahoo.com>
2024-06-04 11:05:13 -0400

    Update datauploading.R/redoing .gitignor
    This database should work using the service account credentials I gave it. I needed to roll back my changes due to not having my .gitignore.

--- datauploading.R
+++ datauploading.R
@@ -4,10 +4,14 @@ library(googlesheets4)
 
 df <- read_csv(here('surveydown/data.csv'))
 
-#credentials <- jsonlite::read_json("secret.json") #Auth stuff ill do later
 
 
 
+
+
+
+#Primary Solution Service Key Auth - Needs Service Email and
+credentials_path <- "documents/GitHub/surveydown/Service.json"
 
 
 
@@ -21,31 +25,37 @@ sheet_url <- "https://docs.google.com/spreadsheets/d/12f5iLaXlXqkWz3pWs6fLLPX6Pf
 
 Sheet <- read_sheet(sheet_url, range = "A:A")
 
-
-create_object <- function() {
-  stop()
-}
-
-
-tryCatch(
-  {
-    sheet_data <- read_sheet(NULL, range = "A:A") #I have to now replace this NULL with get_sheet_url
-  },
-  error = function(e) {
-    Spread1 <- gs4_create("Sheets1", sheets = df)
-  }
-)
-
+#
+#create_object <- function() {
+#  stop()
+#}
+#
+#
+## Sheet ID
+#ssID <- "1cNZeKg_BjN6fTDPOZtSFk8kxt7hVDsSOCo84xu4eW_U"
+#tryCatch(
+#  {
+#    sheet_data <- read_sheet(NULL, range = "A:A") #I have to now replace this NULL with get_sheet_url
+#  },
+#  error = function(e) {
+#    Spread1 <- gs4_create("Sheets1", sheets = df)
+#  }
+#)
+#
 
 
 
 
 rows_to_delete <- which(Sheet$session_id %in% df$session_id)
 
+if(nrow(Sheet) == 0) {
+  range_write(ssID, data = df)
+}
+
 if(length(rows_to_delete) == 0) { #This is meant for first time addition of a row
-  range_write(sheet_url, data = df, range = paste("A", as.character(nrow(Sheet) + 2), sep = ""), col_names = FALSE)
+  range_write(ssID, data = df, range = paste("A", as.character(nrow(Sheet) + 2), sep = ""), col_names = FALSE)
 } else {
-  range_write(sheet_url, data = df, range = paste("A", as.character(rows_to_delete[1] + 1), sep = ""), col_names = FALSE) #We have to add one so we can avoid pasting the header
+  range_write(ssID, data = df, range = paste("A", as.character(rows_to_delete[1] + 1), sep = ""), col_names = FALSE) #We have to add one so we can avoid pasting the header
 }
 
 
