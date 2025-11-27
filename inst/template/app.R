c53849897182c4aebc6574f94f4edfbfb5d28159
emse-p4a-gwu <john.helveston@gmail.com>
2025-11-27 06:05:38 -0500

    few more updates to templates

--- inst/template/app.R
+++ inst/template/app.R
@@ -36,7 +36,7 @@ ui <- sd_ui()
 
 server <- function(input, output, session) {
   # Define any conditional skip logic here (skip to page if a condition is true)
-  sd_skip_forward()
+  sd_skip_if()
 
   # Define any conditional display logic here (show a question if a condition is true)
   sd_show_if()
